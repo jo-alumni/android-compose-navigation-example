02d71b34e81c58dca851cd7dc9dfe3177d3cde33
nakamurajo <u249101d@alumni.osaka-u.ac.jp>
2025-11-04 23:44:41 +0900

    chore: Delete LoadingState sealed interface
    Removed the `LoadingState` sealed interface from the `core` module. This file is no longer needed.

--- core/src/main/kotlin/com/example/navigationtest/core/util/LoadingState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.navigationtest.core.util
-
-sealed interface LoadingState<out T> {
-    data class Success<T>(val data: T) : LoadingState<T>
-    data object Failure : LoadingState<Nothing>
-    data object Loading : LoadingState<Nothing>
-}
