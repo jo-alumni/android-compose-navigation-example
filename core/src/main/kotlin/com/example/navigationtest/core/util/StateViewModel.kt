1c28d49dfcea6a180b5f4920f5134f5c5efc7096
nakamurajo <u249101d@alumni.osaka-u.ac.jp>
2025-11-04 23:11:08 +0900

    feat: Add logging for state and events in StateViewModel
    This commit introduces automatic logging for UI state changes and UI events within the `StateViewModel` base class.
    
    These changes include:
    - An `init` block was added to `StateViewModel`.
    - Inside the `init` block, `viewModelScope` is used to launch coroutines that collect and log `uiState` and `uiEvent` flows.
    - The logs include the simple name of the ViewModel class for easier debugging.

--- core/src/main/kotlin/com/example/navigationtest/core/util/StateViewModel.kt
+++ core/src/main/kotlin/com/example/navigationtest/core/util/StateViewModel.kt
@@ -1,15 +1,25 @@
 package com.example.navigationtest.core.util
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 abstract class StateViewModel<S : State, E : Event>(initialState: S) : ViewModel() {
     protected val _uiState = MutableStateFlow(initialState)
     val uiState: StateFlow<S> = _uiState.asStateFlow()
     protected val _uiEvent = MutableSharedFlow<E>()
     val uiEvent = _uiEvent.asSharedFlow()
+
+    init {
+        viewModelScope.launch {
+            uiState.collect { uiState -> Log.d(this@StateViewModel.javaClass.simpleName, "uiState: $uiState") }
+            uiEvent.collect { uiEvent -> Log.d(this@StateViewModel.javaClass.simpleName, "uiEvent: $uiEvent") }
+        }
+    }
 }
