e5ee5eeb9855f109ff7309250155c65c9a938f08
nakamurajo <u249101d@alumni.osaka-u.ac.jp>
2025-11-08 00:04:19 +0900

    style: Add explicit types in StateViewModel
    This commit adds explicit type declarations to the properties within the `StateViewModel` abstract class. This change improves code clarity and readability by making the types of `mutableUiState`, `uiState`, `mutableUiEvent`, and `uiEvent` explicit, without altering any functionality.

--- core/src/main/kotlin/com/example/navigationtest/core/util/StateViewModel.kt
+++ core/src/main/kotlin/com/example/navigationtest/core/util/StateViewModel.kt
@@ -5,18 +5,18 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 abstract class StateViewModel<S : State, E : Event>(initialState: S) : ViewModel() {
-    protected val mutableUiState = MutableStateFlow(initialState)
+    protected val mutableUiState: MutableStateFlow<S> = MutableStateFlow(initialState)
     val uiState: StateFlow<S> = mutableUiState.asStateFlow()
-    protected val currentState: S
-        get() = mutableUiState.value
-    protected val mutableUiEvent = MutableSharedFlow<E>()
-    val uiEvent = mutableUiEvent.asSharedFlow()
+    protected val currentState: S get() = mutableUiState.value
+    protected val mutableUiEvent: MutableSharedFlow<E> = MutableSharedFlow()
+    val uiEvent: SharedFlow<E> = mutableUiEvent.asSharedFlow()
 
     init {
         viewModelScope.launch {
