882483781c6f3be8587ca31b71a238d682d3232c
nakamurajo <u249101d@alumni.osaka-u.ac.jp>
2026-02-18 23:36:23 +0900

    feat: Implement mutex for thread-safe state updates in PostsViewModel

--- feature/posts/src/main/kotlin/com/example/navigationtest/posts/PostsViewModel.kt
+++ feature/posts/src/main/kotlin/com/example/navigationtest/posts/PostsViewModel.kt
@@ -8,6 +8,8 @@ import com.example.navigationtest.posts.contract.PostsState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,52 +18,58 @@ internal class PostsViewModel @Inject constructor(
 ) : ContractedViewModel<PostsState, PostsEvent>(
     initialState = PostsState.Initial(emptyList()),
 ) {
+    private val mutex = Mutex()
+
     fun refresh() {
-        if (currentState is PostsState.Loading) return
         viewModelScope.launch {
-            _uiState.update { state ->
-                PostsState.Loading(
-                    posts = state.posts,
-                    type = PostsState.Loading.Type.REFRESH,
+            mutex.withLock {
+                if (currentState is PostsState.Loading) return@withLock
+                _uiState.update { state ->
+                    PostsState.Loading(
+                        posts = state.posts,
+                        type = PostsState.Loading.Type.REFRESH,
+                    )
+                }
+                runCatching {
+                    postRepository.getPosts(page = 1, POSTS_LIMIT)
+                }.fold(
+                    onSuccess = {
+                        _uiState.emit(PostsState.Stable(posts = it))
+                        _uiEvent.emit(PostsEvent.ShowSnackbar.Success)
+                    },
+                    onFailure = { cause ->
+                        _uiState.update { state -> PostsState.Error(posts = state.posts, cause = cause) }
+                        _uiEvent.emit(PostsEvent.ShowSnackbar.Error)
+                    },
                 )
             }
-            runCatching {
-                postRepository.getPosts(page = 1, POSTS_LIMIT)
-            }.fold(
-                onSuccess = {
-                    _uiState.emit(PostsState.Stable(posts = it))
-                    _uiEvent.emit(PostsEvent.ShowSnackbar.Success)
-                },
-                onFailure = { cause ->
-                    _uiState.update { state -> PostsState.Error(posts = state.posts, cause = cause) }
-                    _uiEvent.emit(PostsEvent.ShowSnackbar.Error)
-                },
-            )
         }
     }
 
     fun loadMore() {
-        if (currentState is PostsState.Loading) return
         viewModelScope.launch {
-            _uiState.update { state ->
-                PostsState.Loading(
-                    posts = state.posts,
-                    type = PostsState.Loading.Type.LOAD_MORE,
+            mutex.withLock {
+                if (currentState is PostsState.Loading) return@withLock
+                _uiState.update { state ->
+                    PostsState.Loading(
+                        posts = state.posts,
+                        type = PostsState.Loading.Type.LOAD_MORE,
+                    )
+                }
+                runCatching {
+                    val nextPage = (currentState.posts.size / POSTS_LIMIT) + 1
+                    postRepository.getPosts(page = nextPage, POSTS_LIMIT)
+                }.fold(
+                    onSuccess = { newPosts ->
+                        _uiState.emit(PostsState.Stable(posts = currentState.posts + newPosts))
+                        _uiEvent.emit(PostsEvent.ShowSnackbar.Success)
+                    },
+                    onFailure = { cause ->
+                        _uiState.update { state -> PostsState.Error(posts = state.posts, cause = cause) }
+                        _uiEvent.emit(PostsEvent.ShowSnackbar.Error)
+                    },
                 )
             }
-            runCatching {
-                val nextPage = (currentState.posts.size / POSTS_LIMIT) + 1
-                postRepository.getPosts(page = nextPage, POSTS_LIMIT)
-            }.fold(
-                onSuccess = { newPosts ->
-                    _uiState.emit(PostsState.Stable(posts = currentState.posts + newPosts))
-                    _uiEvent.emit(PostsEvent.ShowSnackbar.Success)
-                },
-                onFailure = { cause ->
-                    _uiState.update { state -> PostsState.Error(posts = state.posts, cause = cause) }
-                    _uiEvent.emit(PostsEvent.ShowSnackbar.Error)
-                },
-            )
         }
     }
 
