af15171d6425eb3d4e1ea6272405dee77093e58d
nakamurajo <u249101d@alumni.osaka-u.ac.jp>
2026-02-18 21:45:34 +0900

    feat: Optimize API calls by offloading to IO dispatcher

--- api/src/main/kotlin/com/example/navigationtest/api/datasource/PostApiDataSource.kt
+++ api/src/main/kotlin/com/example/navigationtest/api/datasource/PostApiDataSource.kt
@@ -12,49 +12,65 @@ import io.ktor.client.request.post
 import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.client.request.url
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class PostApiDataSource @Inject constructor(
     private val httpClient: HttpClient,
 ) {
-    suspend fun getPosts(page: Int, limit: Int): List<PostApiModel> = httpClient.get {
-        url(path = "posts")
-        parameter("_page", page)
-        parameter("_limit", limit)
-    }.body()
+    suspend fun getPosts(page: Int, limit: Int): List<PostApiModel> = withContext(Dispatchers.IO) {
+        httpClient.get {
+            url(path = "posts")
+            parameter("_page", page)
+            parameter("_limit", limit)
+        }.body()
+    }
 
-    suspend fun getPost(postId: Int): PostApiModel = httpClient.get {
-        url(path = "posts/$postId")
-    }.body()
+    suspend fun getPost(postId: Int): PostApiModel = withContext(Dispatchers.IO) {
+        httpClient.get {
+            url(path = "posts/$postId")
+        }.body()
+    }
 
     suspend fun deletePost(postId: Int) {
-        httpClient.delete {
-            url(path = "posts/$postId")
+        withContext(Dispatchers.IO) {
+            httpClient.delete {
+                url(path = "posts/$postId")
+            }
         }
     }
 
-    suspend fun getCommentsRelatedPost(postId: Int): List<CommentApiModel> = httpClient.get {
-        url(path = "posts/$postId/comments")
-    }.body()
+    suspend fun getCommentsRelatedPost(postId: Int): List<CommentApiModel> = withContext(Dispatchers.IO) {
+        httpClient.get {
+            url(path = "posts/$postId/comments")
+        }.body()
+    }
 
     suspend fun createPost(post: PostApiModel) {
-        httpClient.post {
-            url(path = "posts")
-            setBody(post)
+        withContext(Dispatchers.IO) {
+            httpClient.post {
+                url(path = "posts")
+                setBody(post)
+            }
         }
     }
 
     suspend fun patchPost(post: PostApiModel) {
-        httpClient.patch {
-            url(path = "posts/${post.id}")
-            setBody(post)
+        withContext(Dispatchers.IO) {
+            httpClient.patch {
+                url(path = "posts/${post.id}")
+                setBody(post)
+            }
         }
     }
 
     suspend fun putPost(post: PostApiModel) {
-        httpClient.put {
-            url(path = "posts/${post.id}")
-            setBody(post)
+        withContext(Dispatchers.IO) {
+            httpClient.put {
+                url(path = "posts/${post.id}")
+                setBody(post)
+            }
         }
     }
 }
