1b716cb32503a254adac6325b0bd8d88beac2365
nakamurajo <u249101d@alumni.osaka-u.ac.jp>
2026-02-21 01:39:39 +0900

    feat: Update build.gradle.kts to integrate Hilt and clean up dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     alias(libs.plugins.navigationTest.app)
     alias(libs.plugins.navigationTest.compose)
     alias(libs.plugins.navigationTest.kotlin)
+    alias(libs.plugins.navigationTest.hilt)
     alias(libs.plugins.ktlint)
-    alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
 }
 
@@ -52,26 +52,14 @@ android {
 }
 
 dependencies {
-    implementation(libs.hilt.android)
-    ksp(libs.hilt.compiler)
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.material.icon)
-    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 
     // core modules
     implementation(projects.core)
