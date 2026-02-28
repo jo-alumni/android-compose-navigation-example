de16ebd7e06a0a81a330f2b013b2c73bb03ac36f
nakamurajo <u249101d@alumni.osaka-u.ac.jp>
2026-02-28 22:18:45 +0900

    feat: Change visibility of PostsTab and TodoState to internal

--- app/feature/posts/src/main/kotlin/com/example/navigationtest/app/feature/posts/PostsTab.kt
+++ app/feature/posts/src/main/kotlin/com/example/navigationtest/app/feature/posts/PostsTab.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.example.navigation_test.app.feature.posts.R
 
-enum class PostsTab {
+internal enum class PostsTab {
     Recommended,
     Followee;
 
--- app/feature/todo/src/main/kotlin/com/example/navigationtest/app/feature/todo/contract/TodoState.kt
+++ app/feature/todo/src/main/kotlin/com/example/navigationtest/app/feature/todo/contract/TodoState.kt
@@ -3,7 +3,7 @@ package com.example.navigationtest.app.feature.todo.contract
 import com.example.navigationtest.app.core.util.State
 import com.example.navigationtest.core.domain.entity.Todo
 
-data class TodoState(
+internal data class TodoState(
     val notDoneTodos: List<Todo> = emptyList(),
     val doneTodos: List<Todo> = emptyList(),
     val input: String = "",
