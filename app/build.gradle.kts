c0597a4e0c0c01d13256874858e9f6625bc12dfe
nakamurajo <u249101d@alumni.osaka-u.ac.jp>
2026-03-01 18:46:17 +0900

    feat: Update target SDK version to 36 in build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
     defaultConfig {
         applicationId = "com.example.navigation_test"
         minSdk = 31
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
